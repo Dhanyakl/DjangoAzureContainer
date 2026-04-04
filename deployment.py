383d92f83b0853cafe45c87e03ab6799e39821f3
Dhanyakl <109303327+Dhanyakl@users.noreply.github.com>
2026-04-05 00:18:39 +0200

    Update import paths for Django Azure Storage

--- deployment.py
+++ deployment.py
@@ -1,6 +1,6 @@
 import os
-from settings.py import *
-from settings import BASE_DIR
+from django_azure_storage.settings.py import *
+from django_azure_storage.settings import BASE_DIR
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
