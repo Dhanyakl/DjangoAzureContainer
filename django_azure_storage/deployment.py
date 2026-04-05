2c6c698c63a98ef5a3ce9d48acd7d8d4515295e4
Dhanyakl <109303327+Dhanyakl@users.noreply.github.com>
2026-04-05 14:59:50 +0200

    deleted postsql part from deploy

--- django_azure_storage/deployment.py
+++ django_azure_storage/deployment.py
@@ -19,16 +19,4 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('=')[0]:pair.split('=')[1] for pair in connection_string.split(' ')}
-
-DATABASES = {
-    'default' : {
-    'ENGINE' : 'django.db.backends.postgresql',
-    'NAME' : parameters['dbname'],
-    'HOST': parameters['host'],
-    'USER': parameters['user'],
-    'PASSWORD':parameters['password'],
- }
-}
 
